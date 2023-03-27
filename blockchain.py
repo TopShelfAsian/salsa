d88451e18074de68aab18837017c0400e350cf82
TopShelfAsian <rjflip@gmail.com>
2023-03-27 12:14:00 -0700

    Fixed from & import errors & seperated the classes.No arguements passed but when run 'python3 blockchain.py' it should make it to 'argv[1] == add'

--- blockchain.py
+++ blockchain.py
@@ -1,70 +1,9 @@
 #!/usr/bin/env python3
-import Node from Node
-import LinkedList from LinkedList
+from Node import Node
+from LinkedList import LinkedList
 import sys
 import getopt
-class Node(object):
-    def __init__(self, previousHash, timestamp, caseID, evidenceID, state, dataLength, data):
-        self.previousHash = previousHash
-        self.timestamp = timestamp
-        self.caseID = caseID
-        self.evidenceID = evidenceID
-        self.state = state
-        self.dataLength = dataLength
-        self.data = data
-        self.next = None
 
-class LinkedList(object):
-    def __init__(self, head=None):
-        self.head = head
-
-    def append(self, newNode):
-        current = self.head
-        if current:
-            while current.next:
-                current = current.next
-            current.next = newNode
-        else:
-            self.head = newNode
-                
-    def delete(self, value):
-        #Delete first note with a given value
-        current = self.head
-        if current.evidenceID == value:
-            self.head = current.next
-        else:
-            while current:
-                if current.evidenceID == value:
-                    break
-                prev = current
-                current = current.next
-            if current == None:
-                return
-            prev.next = current.next
-            current = None
-
-    def insert(self, newElement, position):
-        #insert a new node at given position
-        count = 1
-        current = self.head
-        if position == 1:
-            newElement.next = self.head
-            self.head = newElement
-        while current:
-            if count + 1 == position:
-                newElement.next = current.next
-                current.next = newElement
-                return
-            else:
-                count += 1
-                current = current.next
-        pass
-
-    def print(self):
-        current = self.head
-        while current:
-            print(current.evidenceID)
-            current = current.next
             
 def arginit(argv):
 	i = 2
