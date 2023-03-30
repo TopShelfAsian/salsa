aea44c194c7fb805a97ba6006b32c409e748e7d7
TopShelfAsian <rjflip@gmail.com>
2023-03-30 11:40:33 -0700

    Merged functions in LinkedList.py to be called in Blockchain.py. All functions should be working besides Log.

--- LinkedList.py
+++ LinkedList.py
@@ -17,57 +17,95 @@ class LinkedList(object):
 
 
     def append(self, newNode):
-        current = self.head
-        if current:
-            while current.next:
-                current = current.next
-            current.next = newNode
+        temp = self.head
+        if temp:
+            while temp.next:
+                temp = temp.next
+            temp.next = newNode
         else:
             self.head = newNode
 
     def size(self):
-        current = self.head
+        temp = self.head
         count = 0
-        while current:
+        while temp:
             count += 1
-            current = current.next
+            temp = temp.next
         return count
+    
     def print(self):
-        current = self.head
-        while current != None:
-            print("Previous hash: " + str(current.previousHash) + ", Timestamp: " + str(current.timestamp) +", CaseID: " + str(current.caseID) + ", EvidenceID: " + str(current.evidenceID) + ", State: " + str(current.state) +", Data Length: " + str(current.dataLength) +", Data: " + str(current.data))
-            current = current.next
+        temp = self.head
+        while temp != None:
+            print("Previous hash: " + str(temp.previousHash) + ", Timestamp: " + str(temp.timestamp) +", CaseID: " + str(temp.caseID) + ", EvidenceID: " + str(temp.evidenceID) + ", State: " + str(temp.state) +", Data Length: " + str(temp.dataLength) +", Data: " + str(temp.data))
+            temp = temp.next
         return True
     
     def find(self, target):
-        current = self.head
-        while current:
-            if current == target:
-                return current
+        temp = self.head
+        while temp:
+            if temp == target:
+                return temp
         else:
-            current = current.next
-            
+            temp = temp.next
+
+    #used to check the state of a block        
     def checkstate(self, id):
-        current = self.head
-        while current:
-            if current.evidenceID == id:
-                return current.state
+        temp = self.head
+        while temp:
+            if temp.evidenceID == id:
+                return temp.state
         else:
-            current = current.next
+            temp = temp.next
         return "N/A"
     
+    #Used for add & checking if the evidenceID is already in use
     def checkEID(self, id):
-        current = self.head
-        while current:
-            if current.evidenceID == id:
+        temp = self.head
+        while temp:
+            if temp.evidenceID == id:
                 return True
             else:
-                current = current.next
+                temp = temp.next
         return False
+    
+    def printLog(self):
+        temp = self.head.next
+        while temp:
+            print("Case: " + temp.caseID)
+            print("Item: " + temp.evidenceID)
+            print("Action: " + temp.state)
+            print("Time: " + temp.timestamp + "\n")
+            temp = temp.next
+        return 
+    
+    #https://www.geeksforgeeks.org/python-program-for-reverse-a-linked-list/
+    def reverseLL(self):
+        previous = None
+        temp = self.head
+        while(temp != None):
+            next = temp.next
+            temp.next = previous
+            previous = temp
+            temp = next
+        self.head = previous
+    
+    def log(self, eID, count):
+        log_ll = LinkedList()
+        #made it the node after head bc I don't think we need to print head in log 
+        #This covers the most basic test case that they call log with no flags
+        if count == 0 & eID == None:
+            log_ll = self
+            #reverses the LL so it prints the ALL the LL
+            log_ll.reverseLL()
+            log_ll.printLog()
+            return
+
+
+        return True
 
     def add(self, cID, eID):
         #insert a new block into the blockchain given the caseID & the evidenceID
-        current = self.head
+        temp = self.head
         flag = self.checkEID(eID)
         if flag == True:
             #Error Code will need to be generated for this; for example if they do 'echo $?' then it would need to return our predefined integer value for this error
@@ -87,26 +125,25 @@ class LinkedList(object):
                 
     def remove(self, id):
         #Deletes a node given a specified id
-        current = self.head
-        while current:
-            if current.evidenceID == id:
-                if current.state == "CHECKEDIN":
-                    current.state = "RELEASED"
-                    print("Case:" + str(current.caseID))
-                    print("Removed item: " + str(current.evidenceID))
-                    print("\tStatus: " + str(current.state))
-                    print("\tOwner info: NEED TO IMPLEMENT TO TAKE OWNER INFO FROM ARGS & PLACE HERE")
-                    timeOfAction = datetime.now()
-                    iso_timeOfAction = datetime.isoformat(timeOfAction)
-                    print("\tTime of action: " + str(timeOfAction))
+        temp = self.head
+        while temp:
+            if temp.evidenceID == id:
+                if temp.state == "CHECKEDIN":
+                    temp.state = "RELEASED"
+                    print("Case:" + str(temp.caseID))
+                    print("Removed item: " + str(temp.evidenceID))
+                    print("\tStatus: " + str(temp.state))
+                    #timeOfAction = datetime.now()
+                    #iso_timeOfAction = datetime.isoformat(timeOfAction)
+                    #print("\tTime of action: " + str(timeOfAction))
                     return True
                 else:
                     #Error Code will need to be generated for this; for example if they do 'echo $?' then it would need to return our predefined integer value for this error
-                    current.state = "ERROR"
+                    temp.state = "ERROR"
                     print("Error: Cannot remove an evidence item that is not CHECKEDIN.")
                     return False
             else:
-                current = current.next
+                temp = temp.next
         print("ERROR: NOT FOUND")
         return False
 
@@ -115,26 +152,26 @@ class LinkedList(object):
         #takes the LL as a param and the item_id
         # changes to CHECKEDOUT state for an evidence item given its item_id
         #should display its case(hash), item_id, new status of evidence, time
-        current = self.head
+        temp = self.head
         test = Node
-        while current:
-            if current.evidenceID == id:
-                if current.state == "CHECKEDIN":
-                    current.state = 'CHECKEDOUT'
-                    print("Case: " + str(current.caseID))
-                    print("Checked out Item: " + str(current.evidenceID))
-                    print("\t Status: " + str(current.state))
+        while temp:
+            if temp.evidenceID == id:
+                if temp.state == "CHECKEDIN":
+                    temp.state = 'CHECKEDOUT'
+                    print("Case: " + str(temp.caseID))
+                    print("Checked out Item: " + str(temp.evidenceID))
+                    print("\t Status: " + str(temp.state))
                     timeOfAction = datetime.now()
                     iso_timeOfAction = datetime.isoformat(timeOfAction)
                     print("\tTime of action: " + str(timeOfAction))
                     return True
                 else:
                     #Error Code will need to be generated for this; for example if they do 'echo $?' then it would need to return our predefined integer value for this error
-                    current.state = "ERROR"
+                    temp.state = "ERROR"
                     print("Error: Cannot check out a checked out item. Must check it in first.")
                     return False
             else:
-                current = current.next
+                temp = temp.next
         print("ERROR: NOT FOUND")
         return False
 
@@ -143,25 +180,25 @@ class LinkedList(object):
         #takes the LL as a param and the item_id
         #changes to CHECKEDIN state for an evidence item given its item_id
         #should display its case(hash), item_id, new status of evidence, time
-        current = self.head
-        while current:
-            if current.evidenceID == id:
-                if current.previousHash != None:
-                    current.state = 'CHECKEDIN'
-                    print("Case: " + str(current.caseID))
-                    print("Checked out Item: " + str(current.evidenceID))
-                    print("\t Status: " + str(current.state))
+        temp = self.head
+        while temp:
+            if temp.evidenceID == id:
+                if temp.previousHash != None:
+                    temp.state = 'CHECKEDIN'
+                    print("Case: " + str(temp.caseID))
+                    print("Checked out Item: " + str(temp.evidenceID))
+                    print("\t Status: " + str(temp.state))
                     timeOfAction = datetime.now()
                     iso_timeOfAction = datetime.isoformat(timeOfAction)
                     print("\tTime of action: " + str(timeOfAction))
                     return True
                 else:
                     #Error Code will need to be generated for this; for example if they do 'echo $?' then it would need to return our predefined integer value for this error
-                    current.state = "ERROR"
+                    temp.state = "ERROR"
                     print("Error: Checkin actions may only be performed on evidence items that have already been added to the blockchain. No previous hash found so not added.")
                     return False
             else:
-                current = current.next
+                temp = temp.next
         print("ERROR: NOT FOUND")
         return False
     
@@ -171,18 +208,18 @@ class LinkedList(object):
         #If the state is ERROR then that means at least one block had an error & will need to return: bad blocks hash, & reason for error.
         #Parent block not found, same parent block for more than one block, block contents do not match block checksum, trying to checkin/checkout after removing block
         num = self.size()
-        current = self.head
+        temp = self.head
         print("Transactions in blockchain: " + str(num))
-        while current:
+        while temp:
             #Do we print all errors or just first one found?
-            if current.state == "ERROR":
-                print("State of blockchain: " + current.state)
+            if temp.state == "ERROR":
+                print("State of blockchain: " + temp.state)
                 #needs to be a hash with no hyphens in it
-                print("Bad block: " + current.previousHash)
+                print("Bad block: " + temp.previousHash)
                 #needs to see the reason for error - possibly add that in to node struct
                 print("NEED TO IMPLEMENT REASON - if parent block issue need to list it's hash(If it has one!) otherwise just print the error")
                 return False
             else:
-                current = current.next
+                temp = temp.next
         print("State of blockchain: CLEAN") 
         return True
--- blockchain.py
+++ blockchain.py
@@ -9,6 +9,7 @@ def arginit(argv):
 	i = 2
 	itemIDs = []
 	if argv[1] == "add":
+		add_ll = LinkedList()
 		if len(argv) > 2:
 			if argv[i] == "-c":
 				caseID = argv[i+1]
@@ -17,12 +18,15 @@ def arginit(argv):
 				numitems = 0
 				while i < len(argv):
 					if argv[i] == "-i":
-						itemIDs.append(argv[i+1])
-						print("Added item: " + itemIDs[numitems])
-						numitems = numitems+1
+						#evidenceID should be i+1
+						#add() should handle all the prints necessary for each evidenceID provided & does the check for the evidenceID existing in LL
+						add_ll.add(caseID, i+1)
+						#itemIDs.append(argv[i+1])
+						#print("Added item: " + itemIDs[numitems])
+						#numitems = numitems+1
 						#TO DO: Add caseID, itemID to blockchain Node, establish Node
-						print(" Status: ")
-						print(" Time of action: ")
+						#print(" Status: ")
+						#print(" Time of action: ")
 					else:
 						exit(3)
 					i = i+2
@@ -32,66 +36,81 @@ def arginit(argv):
 		else:
 			exit(3)	
 	elif argv[1] == "checkout":
+		checkout_ll = LinkedList()
 		if len(argv) > 2:
 			if argv[i] == "-i":
-				checkedOut = False;
+				evidenceID = argv[i+1]
+				#checkout() handles all the prints & error checking
+				#TO CHECK: should we be throwing the exit(1) from the LinkedList class instead of returing False?
+				checkout_ll.checkout(evidenceID)
+				#checkedOut = False
 				#Fetch caseID of block with matching itemID and block info
-				print("Case: ")
-				
-				if checkedOut == False:
-					print("Checked out item: " + argv[i+1])
-					print(" Status: ")
-					print(" Time of action: ")
-				else:
-					print("Error: Cannot check out a checked out item. Must check in first.")
-					exit(1)
+				#print("Case: ")
+				#if checkedOut == False:
+				#	print("Checked out item: " + argv[i+1])
+				#	print(" Status: ")
+				#	print(" Time of action: ")
+				#else:
+				#	print("Error: Cannot check out a checked out item. Must check in first.")
+				#	exit(1)
 			else:
 				exit(3)
 		else:
 			exit(3)
 		
 	elif argv[1] == "checkin":
+		checkin_ll = LinkedList()
 		if len(argv) > 2:
 			if argv[i] == "-i":
-				#Fetch caseID of block with matching itemID and block info
-				print("Case: ")
-				print("Checked in item: " + argv[i+1])
-				print(" Status: ")
-				print(" Time of action: ")
+				#Fetched evidenceID of block with matching itemID and block info
+				evidenceID = argv[i+1]
+				#checkin() handles all the prints & error checking
+				#ALTHOUGH, I am unsure if previousHash refers to previous node in LL, which is the check if the evidenceID passed has already been added to blockchain
+				checkin_ll.checkin(evidenceID)
+				#print("Case: ")
+				#print("Checked in item: " + argv[i+1])
+				#print(" Status: ")
+				#print(" Time of action: ")
 			else:
 				exit(3)
 		else:
 			exit(3)
 	elif argv[1] == "log":
+		log_ll = LinkedList()
 		if len(argv) > 2:
-			if argv[i] == "-r": # Reverse order of log entries printed
-				#Fetch caseID of block with matching itemID and block info
-				print("Case: ")
-				print("Checked in item: ")
-				print(" Status: ")
-				print(" Time of action: ")
+			if argv[i] == "-r": # Reverse order of log entries printed. This will be newest to oldest.
+			#Fetch caseID of block with matching itemID and block info
+				log_ll.printLog()	
 			#elif argv[i] == "-n": Set number of log entries
 			
 			#elif argv[i] == "-i": # Find log entries with matched itemID
+				evidenceID = i+1
+				log_ll.log(evidenceID, 0)
 			
 			#elif argv[i] == "-c": # Find log entries with matched caseID
 			
-			#else: # Print all contents of the log.
+			#else: # Print all contents of the log. This will be in the oldest to newest changes
+				log_ll.log(None, 0)
 		else:
 			exit(3)
 	elif argv[1] == "remove":
+		remove_ll = LinkedList()
 		if len(argv) > 2:
 			if argv[i] == "-i":
-				caseID = 0 # set to blockchain node with matching itemID
+				evidenceID = 0 # set to blockchain node with matching itemID - need this to be grabbed from arguements so that remove() knows what it's looking for
 				i = i+2
 				if argv[i] == "-y":
-					print("Case: ")
-					print("Removed item:" + argv[i-1])
-					print(" Status: ")
+					#remove() will print ther caseID, evidenceID, & new status, & handle the error check of it being checkin
+					remove_ll.remove(evidenceID)
+					#print("Case: ")
+					#print("Removed item:" + argv[i-1])
+					#print(" Status: ")
 					i = i+2
 					if argv[i] == "-o":
-						print(" Owner info: " + argv[i+1])
-					print(" Time of action: ")
+						print("\tOwner info: NEED TO IMPLEMENT TO TAKE OWNER INFO FROM ARGS & PLACE HERE")
+					timeOfAction = datetime.now()
+					iso_timeOfAction = datetime.isoformat(timeOfAction)
+					print("\tTime of action: " + str(timeOfAction))
 				else:
 					exit(3)
 					
@@ -100,37 +119,20 @@ def arginit(argv):
 		else:
 			exit(3)	
 	elif argv[1] == "init":
+		init_ll = LinkedList()
+		#init() does all the checks 
+		init_ll.init()
 		#Check if head node is made.
-		nodeFound = False
-		if nodeFound == False:
-			print("Blockchain file not found. Created INITIAL block.")
-		else:
-			print("Blockchain file found with INITIAL block.")
+		#nodeFound = False
+		#if nodeFound == False:
+		#	print("Blockchain file not found. Created INITIAL block.")
+		#else:
+		#	print("Blockchain file found with INITIAL block.")
 			
 	elif argv[1] == "verify":
-		numNodes = 0 # Check blockchain file and find number of Nodes.
-		status = "CLEAN" # Check blockchain to see if it has a parent, if two blocks have the same parent, if the contents do not match block checksum, or if an item was checked out or checked in after the chain was removed.
-		print("Transactions in blockchain: " + str(numNodes))
-		print("State of blockchain: " + status)
-		if status == "ERROR":
-			print("Bad block: ")
-	
-	print("Testing for group!")
-	var = input("PLease input a number: ")
-	e1 = Node(1,1,1,1,1,1,1)
-	e2 = Node(2,2,2,2,2,2,2)
-	e3 = Node(3,3,3,3,3,3,3)
-	e4 = Node(4,4,4,4,4,4,4)
-
-	ll = LinkedList(e1)
-	ll.append(e2)
-	ll.append(e3)
-
-	print("Print 3", ll.head.next.next.evidenceID)
-
-	ll.insert(e4,3)
-	ll.delete(1)
-	print(ll.print())
+		verify_ll = LinkedList()
+		#Handles all the logic and print - NEED TO GRAB REASON WHY IT'S BAD BLOCK
+		verify_ll.verify()
 
 def main():
     arginit(sys.argv)
