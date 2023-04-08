74cf69b96b0426247abf5471610ee831e51dbbe3
megaminimush <72623428+megaminimush@users.noreply.github.com>
2023-04-08 08:57:32 -0700

    Added to add() and init() to match more test cases. Still need to work on the added block info.

--- blockchain.py
+++ blockchain.py
@@ -34,6 +34,9 @@ def arginit(argv):
 				caseID = argv[i+1]
 				i = i+2
 				numitems = -1
+				if i >= len(argv):
+					print("No evidence ID given, aborted.")
+					exit(1) # no evidence id given, not possible
 				while i < len(argv):
 					if argv[i] == "-i":
 						itemFound = False
@@ -65,8 +68,7 @@ def arginit(argv):
 										itemFound = True
 						except FileNotFoundError as e:
 								fileWrite = open(filePath, 'wb')
-								currTime = datetime.utcnow()
-								inits = BC_STATS(str.encode("None"), datetime.timestamp(currTime), str.encode("None"), 0, str.encode("INITIAL"), 14)
+								inits = BC_STATS(str.encode(""), 0, str.encode(""), 0, str.encode("INITIAL"), 14)
 								initd = BC_DATA(str.encode("Initial block"))
 								inits = block_layout.pack(*inits)
 								initd = block_datainit_layout.pack(*initd)
@@ -83,12 +85,10 @@ def arginit(argv):
 								newcaseID = caseID[:8]+'-'+caseID[8:12]+'-'+caseID[12:16]+'-'+caseID[16:20]+'-'+caseID[20:]
 								print("Case: " + newcaseID)
 								caseInt = int(caseID, 16)
-								caseInt = int(caseInt)
 							else:
 								newcaseID = caseID
 								caseID = caseID.replace('-', '')
 								caseInt = int(caseID, 16)
-								caseInt = int(caseInt)
 								print("Case: " + newcaseID)
 							print("Added item: " + itemIDs[numitems])
 							#TO DO: Add caseID, itemID to blockchain Node, establish Node
@@ -96,12 +96,10 @@ def arginit(argv):
 							currTime = datetime.utcnow()
 							print("\tTime of action: " + currTime.strftime("%Y-%m-%dT%H:%M:%S.%fZ"))
 							if initOnly == True:
-								block256 = hashlib.sha256()
-								
-								block256.update(bcstatsinit+bcdatainit)
 								fileWrite = open(filePath, 'ab') #APPEND to bc file, must use 'ab'
-								secondst = BC_STATS(str.encode(block256.hexdigest()), datetime.timestamp(currTime), str.encode(str(caseInt)), int(itemIDs[numitems]), str.encode("CHECKEDIN"), 0)
+								secondst = BC_STATS(str.encode(""), datetime.timestamp(currTime), str.encode(str(caseInt)), int(itemIDs[numitems]), str.encode("CHECKEDIN"), 0)
 								secondst = block_layout.pack(*secondst)
+								print(secondst)
 								fileWrite.write(secondst)
 								fileWrite.close()
 							else:
@@ -112,17 +110,22 @@ def arginit(argv):
 
 								blockst = BC_STATS(str.encode(block256.hexdigest()), datetime.timestamp(currTime), str.encode(str(caseInt)), int(itemIDs[numitems]), str.encode("CHECKEDIN"), 0)
 								blockst = block_layout.pack(*blockst)
+								print(blockst)
 								fileWrite.write(blockst)
 								fileWrite.close()
+						else:
+							print("Trying to add duplicate block, aborted.")
+							exit(1)
 							
 					else:
-						exit(3)
+						exit(1)
 					i = i+2
 					
 			else:
-				exit(3)
+				exit(1)
 		else:
-			exit(3)	
+			exit(1)	
+			
 	elif argv[1] == "checkout":
 		if len(argv) > 2:
 			if argv[i] == "-i":
@@ -138,9 +141,9 @@ def arginit(argv):
 					print("Error: Cannot check out a checked out item. Must check in first.")
 					exit(1)
 			else:
-				exit(3)
+				exit(1)
 		else:
-			exit(3)
+			exit(1)
 		
 	elif argv[1] == "checkin":
 		if len(argv) > 2:
@@ -151,12 +154,13 @@ def arginit(argv):
 				print(" Status: ")
 				print(" Time of action: ")
 			else:
-				exit(3)
+				exit(1)
 		else:
-			exit(3)
+			exit(1)
+			
 	elif argv[1] == "log":
 		if len(argv) > 2:
-			if argv[i] == "-r": # Reverse order of log entries printed
+			if argv[i] == "-r" or argv[i] == "--reverse": # Reverse order of log entries printed
 				#Fetch caseID of block with matching itemID and block info
 				print("Case: ")
 				print("Checked in item: ")
@@ -170,44 +174,42 @@ def arginit(argv):
 			
 			#else: # Print all contents of the log.
 		else:
-			exit(3)
+			exit(1)
+			
 	elif argv[1] == "remove":
 		if len(argv) > 2:
 			if argv[i] == "-i":
 				caseID = 0 # set to blockchain node with matching itemID
 				i = i+2
-				if argv[i] == "-y":
+				if argv[i] == "-y" or argv[i] == "--why" :
 					print("Case: ")
 					print("Removed item:" + argv[i-1])
 					print(" Status: ")
 					i = i+2
-					if argv[i] == "-o":
-						print(" Owner info: " + argv[i+1])
-					print(" Time of action: ")
+					if i < len(argv):
+						if argv[i] == "-o":
+							print(" Owner info: " + argv[i+1])
+							print(" Time of action: ")
+						else:
+							exit(1)
 				else:
-					sys.exit(3)
+					sys.exit(1)
 					
 			else:
-				exit(3)
+				exit(1)
 		else:
-			exit(3)	
+			exit(1)	
+			
 	elif argv[1] == "init":
+		if i < len(argv): # no arguments allowed after "init"
+			print("Too many parameters for init")
+			exit(1)
 		#Check if head node is made.
 		try:
 			fileRead = open(filePath, 'rb')
-			bcstats = fileRead.read(block_layout.size)
-			bcdata = fileRead.read(block_datainit_layout.size)
-			bcstats = BC_STATS._make(block_layout.unpack(bcstats))
-			print(bcstats)
-			bcdata = BC_DATA._make(block_datainit_layout.unpack(bcdata))
-			print(bcdata)
-			fileRead.close()
-			print("Blockchain file found with INITIAL block.")
-			exit(0)
 		except FileNotFoundError as e:
 			fileWrite = open(filePath, 'wb')
-			currTime = datetime.utcnow()
-			inits = BC_STATS(str.encode("None"), datetime.timestamp(currTime), str.encode("None"), 0, str.encode("INITIAL"), 14)
+			inits = BC_STATS(str.encode(""), 0, str.encode(""), 0, str.encode("INITIAL"), 14)
 			initd = BC_DATA(str.encode("Initial block"))
 			inits = block_layout.pack(*inits)
 			initd = block_datainit_layout.pack(*initd)
@@ -216,7 +218,43 @@ def arginit(argv):
 			fileWrite.close()
 			print("Blockchain file not found. Created INITIAL block.")
 			exit(0)
-
+		else:
+			invalidFile = True
+			bcstats = fileRead.read(block_layout.size)
+			bcdata = fileRead.read(block_datainit_layout.size)
+			if len(bcstats) != block_layout.size or len(bcdata) != block_datainit_layout.size: # entire init block is not big enough
+				fileWrite = open(filePath, 'wb')
+				inits = BC_STATS(str.encode(""), 0, str.encode(""), 0, str.encode("INITIAL"), 14)
+				initd = BC_DATA(str.encode("Initial block"))
+				inits = block_layout.pack(*inits)
+				initd = block_datainit_layout.pack(*initd)
+				fileWrite.write(inits)
+				fileWrite.write(initd)
+				fileWrite.close()
+				print("Invalid INITIAL block, Created new INITIAL block.")
+				exit(1)
+			else:
+				bcstats = BC_STATS._make(block_layout.unpack(bcstats))
+				#print(bcstats)
+				bcdata = BC_DATA._make(block_datainit_layout.unpack(bcdata))
+				#print(bcdata)
+				if bcdata.Data == b'Initial block\x00' and bcstats.Data_Length == 14: # contents of init block are correct
+					invalidFile = False
+				if invalidFile == True:
+					fileWrite = open(filePath, 'wb')
+					inits = BC_STATS(str.encode(""), 0, str.encode(""), 0, str.encode("INITIAL"), 14)
+					initd = BC_DATA(str.encode("Initial block"))
+					inits = block_layout.pack(*inits)
+					initd = block_datainit_layout.pack(*initd)
+					fileWrite.write(inits)
+					fileWrite.write(initd)
+					fileWrite.close()
+					print("Invalid INITIAL block, Created new INITIAL block.")
+					exit(1)	
+				else:
+					fileRead.close()
+					print("Blockchain file found with INITIAL block.")
+					exit(0)	
 			
 	elif argv[1] == "verify":
 		numNodes = 0 # Check blockchain file and find number of Nodes.
